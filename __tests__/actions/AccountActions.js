01a62ab9011d7ad57922e64fc133e9d0ed09d222
Kevin Wang <rocwang0316@gmail.com>
2019-05-05 12:02:56 -0700

    Added the test code for AccountActions

---
+++ __tests__/actions/AccountActions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { FETCH_ALL_ACCOUNT_SUCCESS } from '../../app/actions/ActionTypes';
+import { FETCH_ALL_ACCOUNT_API } from '../../app/config';
+
+import * as AccountActions from '../../app/actions/AccountActions';
+
+const axiosMock = new MockAdapter(axios);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('AccountActions', () => {
+  test('fetchAllAccount', async () => {
+    const accounts = { id1: {}, id2: { accountId: 'id2' } };
+    axiosMock.onGet(FETCH_ALL_ACCOUNT_API).reply(200, accounts);
+    const expectedActions = [{ type: FETCH_ALL_ACCOUNT_SUCCESS, accounts }];
+
+    const store = mockStore();
+    await store.dispatch(AccountActions.fetchAllAcount());
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+});
